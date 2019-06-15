b77f8c9cf842f73b07170acecc639369a637f2f8
Eddie Perez <eddie.e.perez4@gmail.com>
2019-06-15 16:00:30 -0700

    initial commit

---
+++ stock.js
@@ -0,0 +1,51 @@
+/* globals Chart:false, feather:false */
+
+function renderChart(response) {
+  'use strict'
+
+  feather.replace();
+  let stockSample = 3000;
+
+  // Graphs
+  var ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ],
+      datasets: [{
+        data: [
+          stockSample,
+          stockSample,
+          stockSample,
+          stockSample
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+}
